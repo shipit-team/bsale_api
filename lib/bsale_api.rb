ef57cde52a2c7b607a7514caa46d3b5546fc862e
michel <michel.szinavel@gmail.com>
2017-02-22 19:05:03 -0300

    setting up different endpoints to retreive client information - fixing minnor issues with POST endpoints - added dotenv gem for retreive necessary variables - added more endpoints and test

--- lib/bsale_api.rb
+++ lib/bsale_api.rb
@@ -1,11 +1,12 @@
-require "bsale_api/version"
+require 'bsale_api/version'
 require 'httparty'
+require 'dotenv/load'
 
 class BsaleApi
   include HTTParty
   base_uri 'https://api.bsale.cl'
 
-  def initialize(version=1, extension='json')
+  def initialize(version=1, extension='.json')
     @headers = {
       'Content-Type' => 'application/json',
       'access_token' => ENV['BSALE_TOKEN']
@@ -15,34 +16,42 @@ class BsaleApi
   end
 
   def clients
-    self.class.get("/v#{@version}/clients.#{@extension}", headers: @headers)
+    self.class.get("/v#{@version}/clients#{@extension}", headers: @headers)
   end
 
   def document(data, parameters)
-    self.class.send(parameters[:method].downcase, "/v#{@version}/documents.#{@extension}", headers: @headers, body: data.to_json)
+    self.class.send(parameters[:method].downcase, "/v#{@version}/documents#{@extension}", headers: @headers, body: data.to_json)
   end
 
   def documents
-    self.class.get("/v#{@version}/documents.#{@extension}", headers: @headers)
+    self.class.get("/v#{@version}/documents#{@extension}", headers: @headers)
   end
 
   def document_types
-    self.class.get("/v#{@version}/document_types.#{@extension}", headers: @headers)
+    self.class.get("/v#{@version}/document_types#{@extension}", headers: @headers)
   end
 
-  def office(data, parameters)
-    self.class.send(parameters[:method].downcase, "/v#{@version}/offices.#{@extension}", headers: @headers, body: data.to_json)
+  def office(parameters, data = {})
+    self.class.send(parameters[:method].downcase,
+                    "/v#{@version}/offices#{parameters[:specific]}#{@extension}",
+                    headers: @headers, body: data.to_json)
   end
 
   def offices
-    self.class.get("/v#{@version}/offices.#{@extension}", headers: @headers)
+    self.class.get("/v#{@version}/offices#{@extension}", headers: @headers)
   end
 
-  def shipping(data, parameters)
-    self.class.send(parameters[:method].downcase, "/v#{@version}/shippings.#{@extension}", headers: @headers, body: data.to_json)
+  def shipping(parameters, data = {})
+    self.class.send(parameters[:method].downcase,
+                    "/v#{@version}/shippings#{parameters[:specific]}#{@extension}",
+                    headers: @headers, body: data.to_json)
   end
 
   def shippings
-    self.class.get("/v#{@version}/shippings.#{@extension}", headers: @headers)
+    self.class.get("/v#{@version}/shippings#{@extension}", headers: @headers)
+  end
+
+  def shipping_types
+    self.class.get("/v#{@version}/shipping_types#{@extension}", headers: @headers)
   end
 end
